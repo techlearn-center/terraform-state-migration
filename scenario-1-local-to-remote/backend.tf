8254d718adf838c33a222e30ef40524d04aeab96
techlearn-center <techlearningcenter23@gmail.com>
2026-01-19 17:37:47 -0500

    Fix deprecated S3 backend parameters for LocalStack compatibility

--- scenario-1-local-to-remote/backend.tf
+++ scenario-1-local-to-remote/backend.tf
@@ -18,10 +18,13 @@
 #     region = "us-east-1"
 #
 #     # For LocalStack only - remove for real AWS
-#     endpoint                    = "http://localhost:4566"
+#     endpoints = {
+#       s3 = "http://localhost:4566"
+#     }
 #     skip_credentials_validation = true
 #     skip_metadata_api_check     = true
-#     force_path_style            = true
+#     skip_requesting_account_id  = true
+#     use_path_style              = true
 #     access_key                  = "test"
 #     secret_key                  = "test"
 #   }
